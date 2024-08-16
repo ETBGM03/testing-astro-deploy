5019bed77561fea33911ede75a210705d5f3cc33
Esteban <esfergo150618@gmail.com>
2024-08-16 18:28:26 -0500

    Home page

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+import bgHeader from "./bg_header.jpg";
+import whatsapp from "./icons/whatsapp.svg";
+import serviceCar from "./icons/service_car.svg";
+import road from "./icons/road.svg";
+
+export { bgHeader, whatsapp, serviceCar, road };
