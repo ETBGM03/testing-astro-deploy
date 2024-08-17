d7c894d0d145d6ac314fceb09262dffa394d6472
Esteban <esfergo150618@gmail.com>
2024-08-17 14:38:05 -0500

    service page

---
+++ src/pages/services/constant.ts
@@ -0,0 +1,35 @@
+export const images = [
+  {
+    source: "/assets/gallery/gallery_1.png",
+    textOverlay: "General Vehicle Maintenance",
+  },
+  {
+    source: "/assets/gallery/gallery_2.png",
+    textOverlay: "Mechanical Repairs",
+  },
+  {
+    source: "/assets/gallery/gallery_3.png",
+    textOverlay: "Repair of Electrical and Electronic Systems",
+  },
+  {
+    source: "/assets/gallery/gallery_4.png",
+    textOverlay: "Alignment and Balancing Services",
+  },
+  { source: "/assets/gallery/gallery_5.png", textOverlay: "Tire Services" },
+  {
+    source: "/assets/gallery/gallery_6.png",
+    textOverlay: "Air Conditioning Services",
+  },
+  {
+    source: "/assets/gallery/gallery_7.png",
+    textOverlay: "Body and Paint Services",
+  },
+  {
+    source: "/assets/gallery/gallery_8.png",
+    textOverlay: "Problem Diagnosis and Analysis",
+  },
+  {
+    source: "/assets/gallery/gallery_9.png",
+    textOverlay: "Specialized Services",
+  },
+];
