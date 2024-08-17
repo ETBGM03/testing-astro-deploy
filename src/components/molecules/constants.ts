d9202a633e17c9c7bb198a069ada87cfa7423bd5
Esteban <esfergo150618@gmail.com>
2024-08-17 15:08:41 -0500

    contact page

---
+++ src/components/molecules/constants.ts
@@ -0,0 +1,22 @@
+export const dataHeaderLinks = [
+  {
+    id: 1,
+    to: "/",
+    name: "Home",
+  },
+  {
+    id: 2,
+    to: "/about",
+    name: "about",
+  },
+  {
+    id: 3,
+    to: "/services",
+    name: "Services",
+  },
+  {
+    id: 4,
+    to: "/contact",
+    name: "Contact",
+  },
+];
