3b10bda97576017d802cb35da8dfbfedc03b9360
Esteban <esfergo150618@gmail.com>
2024-08-22 15:14:33 -0500

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  compressHTML: true,
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
+  output: "server",
+  adapter: netlify(),
   integrations: [tailwind()],
 });
